2f04981e1088170c97b67f68bb13659206aa8581
Garvit Khurana <garvitkhurana125@gmail.com>
2026-03-20 19:54:00 +0530

    Corrected Server.js for backend issues

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(
                     "https://expense-tracker-one-xi-70.vercel.app"
         ],
         credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
