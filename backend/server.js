5a815d41188e903d01ca9eb0cee505d229a6a522
Garvit Khurana <garvitkhurana125@gmail.com>
2026-03-20 19:04:02 +0530

    Corrected Server.js for backend issues

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(
                     process.env.CLIENT_URL || "*",
                     "https://your-frontend-name.vercel.app"
         ],
+        credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
